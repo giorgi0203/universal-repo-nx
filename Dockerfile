a8bed20e25e2b7c7bef95bdd31bb19479ec4f974
giorgi <giorgi1997arabuli@gmail.com>
2021-07-28 11:05:49 -0700

    feat(setup) project setup

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM node:12.18-alpine
-
 WORKDIR /usr/src/app
-COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
-RUN npm install --production --silent && mv node_modules ../
+COPY "package.json" ./
+RUN npm install
 COPY . .
 EXPOSE 4200
 CMD ["npm", "start"]
--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'universal-repo-nx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  scrollElements!: NodeListOf<HTMLElement>;
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    this.handleScrollAnimation();
+  }
+  constructor(private r2: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    this.scrollElements = document.querySelectorAll<HTMLElement>(".fade-in-bottom");
+    this.scrollElements.forEach((el) => {
+      el.style.opacity = '0';
+    })
+  }
+
+  inView(el: HTMLElement, percentageScroll = 100) {
+
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+      elementTop <=
+      ((window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100))
+    );
+
+  }
+
+  displayScrollElement(element: HTMLElement) {
+    element.classList.add("scrolled");
+  };
+
+  hideScrollElement(element: HTMLElement) {
+    element.classList.remove("scrolled");
+  };
+
+  handleScrollAnimation() {
+    this.scrollElements.forEach((el) => {
+      if (this.inView(el, 100)) {
+        this.displayScrollElement(el);
+      } else {
+        this.hideScrollElement(el);
+      }
+    })
+  }
+
+  /**
+ * initializes test component
+ * @example
+ * @param1 param 1 is function parama
+ * @returns `string`
+ */
+  initTestComponent() {
+    return '';
+  }
 }
