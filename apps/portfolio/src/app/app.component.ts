901351e1758012d413edc3c472ff250ad8c9c9c5
giorgi <giorgi1997arabuli@gmail.com>
2023-03-03 19:25:16 +0400

    preuprgade commit

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'universal-repo-nx-root',
+  selector: 'portfolio-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
---
+++ apps/portfolio/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-navbar',
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent  {
+
+  // constructor() { }
+
+  // ngOnInit() {
+  // }
+
+}
